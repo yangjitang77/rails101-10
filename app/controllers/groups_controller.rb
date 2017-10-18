1e0898eb2fa4651f82026f4fefa5f11bb7bd475f
yangjitang77 <yangjitang77@gmail.com>
2017-10-18 16:07:31 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create,:edit,:update, :destroy]
+  before_action :authenticate_user!, only: [:new,:create,:edit,:update, :destroy,:join, :quit]
   before_action :find_group_and_check_permission, only: [:edit,:update, :destroy]
   def index
     @groups = Group.all
@@ -13,6 +13,7 @@ class GroupsController < ApplicationController
       @group = Group.new(group_params)
       @group.user = current_user
     if   @group.save
+        current_user.join!(@group)
         redirect_to groups_path
     else
       render :new
@@ -42,6 +43,36 @@ class GroupsController < ApplicationController
     flash[:alert] = "Group deleted"
   end
 
+
+
+    def join
+      @group = Group.find(params[:id])
+
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "加入本讨论版成功！"
+      else
+        flash[:warning] = "你已经是本讨论版成员了"
+      end
+
+      redirect_to group_path(@group)
+    end
+
+    def quit
+      @group = Group.find(params[:id])
+
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:notice] = "退出讨论版成功"
+      else
+        flash[:warning] = "你不是讨论版成员，如何退出"
+      end
+
+      redirect_to group_path(@group)
+
+    end
+
+
   private
 
   def find_group_and_check_permission
@@ -53,7 +84,8 @@ class GroupsController < ApplicationController
 
   end
 
-  def group_params
+
+    def group_params
     params.require(:group).permit(:title,:description)
   end
 end
